8234992111e38ed7e8ad69d5dfd1f485d98a6dfd
Lesa Ducksworth <lducks67@gmail.com>
2020-05-18 22:10:39 -0500

    added number of dice sides input box, added image

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,44 +3,54 @@ let index = 0
 
 const rollDiceButton = document.querySelector('#rollDice')
 const numDiceInput = document.querySelector('#howManyDice')
-const rollTotalOutput = document.querySelector('#numRollTotal')
+let rollTotalOutput = document.querySelector('#numRollTotal')
 const allRollsButton = document.querySelector('#showAll')
 const allRollsContainer = document.querySelector('.allRollsContainer')
 const eachRollList = document.querySelector('#eachRoll')
 const resetButton = document.querySelector('#reset')
 
+// js variable for 'how many sides on dice' input box
+let diceSidesInput = document.querySelector('#howManyDiceSides')
+
 let numDiceSides = 6
 let numRolls
 let rollCount = 0
 let sumDiceRolls
-let removeSingleRollList
 
 rollDiceButton.addEventListener('click', function () {
     console.log('roll button clicked')
     numRolls = (numDiceInput.value)
     console.log(numRolls)
-
+    rollCount = 0
 
     while (rollCount < numRolls) {
 
+        if (diceSidesInput.value >= 6 && diceSidesInput.value <= 20) {
+            let eachDieRoll = (Math.ceil(Math.random() * diceSidesInput.value))
+            console.log(eachDieRoll)
 
-        let eachDieRoll = (Math.floor(Math.random() * numDiceSides) + 1)
-        console.log(eachDieRoll)
+            dieRolls.push(eachDieRoll)
+            console.log(dieRolls)
 
-        dieRolls.push(eachDieRoll)
-        console.log(dieRolls)
+            sumDiceRolls = dieRolls.reduce(function (total, amount) {
+                return total + amount 
+            })
+            
+            console.log(sumDiceRolls);
 
-        sumDiceRolls = dieRolls.reduce(function (total, amount) {
-            return total + amount
-
-        })
+        } else {
+            diceSidesInput.value = ' '
+            numDiceInput.value = ' '
+            sumDiceRolls = 0
+            numRolls = 0
+        }
 
         rollCount++
 
-        console.log(sumDiceRolls)
         rollTotalOutput.innerHTML = sumDiceRolls
+        console.log(rollTotalOutput.innerHTML);
+        
     }
-
 })
 
 allRollsButton.addEventListener('click', function () {
@@ -69,6 +79,7 @@ resetButton.addEventListener('click', function () {
     eachRollList.innerHTML = ' '
     rollCount = 0
     index = 0
-
+    diceSidesInput.value = ' '
 })
+
 
